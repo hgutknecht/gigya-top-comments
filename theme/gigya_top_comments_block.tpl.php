a829fc2eb4f9a563e06a5481371c6757efbf955c
Louis Jimenez <louis.jimenez@mlssoccer.com>
2013-05-22 09:56:50 -0700

    Added error message for when a node id cannot be matched to a stream id

--- theme/gigya_top_comments_block.tpl.php
+++ theme/gigya_top_comments_block.tpl.php
@@ -7,11 +7,17 @@
   if (!empty($query_content)){
     foreach ($query_content as $stream) {
       $node_content = node_load($stream->sid);
-      $node_title = $node_content->title;
-      $node_path = 'node/' . $stream->sid;
-      print '<li class="top_content_item">';
+      if ($node_content) {
+        $node_title = $node_content->title;
+        $node_path = 'node/' . $stream->sid;
+        print '<li class="top_content_item">';
         print '<span class="comment_count">' . l($stream->comment_count, $node_path) . '</span>';
         print '<div class="meta"><span class="node_title">' . l($node_title, $node_path) . '</span>';
+      }
+      else {
+        print '<li class="top_content_item">';
+        print '<div class="meta"><span class="node_title">Content not found. Stream ID ' . $stream->sid . ' does not match any node ID\'s.</span>'; 
+      }
       if ($i == $length - 1) {
         print '</div></li>';
       }

d6a1d5ff56e160799018a92b88759ae83ab23451
Louis Jimenez <louis.jimenez@mlssoccer.com>
2013-11-08 11:27:16 -0500

    Add GA tracking and custom block title

--- theme/gigya_top_comments_block.tpl.php
+++ theme/gigya_top_comments_block.tpl.php
@@ -3,16 +3,19 @@
   print '<ul id="gigya-top-comments">';
   $i = 0;
   $length = count($query_content);
+  $queue = variable_get('gigya_top_comments_google_analytics_queue', '');
   
-  if (!empty($query_content)){
+  if (!empty($query_content)) {
     foreach ($query_content as $stream) {
       $node_content = node_load($stream->sid);
       if ($node_content) {
+        $item = addslashes(htmlspecialchars(url('node/' . $stream->sid)));
         $node_title = $node_content->title;
         $node_path = 'node/' . $stream->sid;
+        $event_tracking = $queue ? 'onClick="_gaq.push([\'_trackEvent\',\''. $queue . '\',\'Position-' . $i . '\',\'' . $item . '\']);"': '';
         print '<li class="top_content_item">';
-        print '<span class="comment_count">' . l($stream->comment_count, $node_path) . '</span>';
-        print '<div class="meta"><span class="node_title">' . l($node_title, $node_path) . '</span>';
+        print '<span class="comment_count" ' . $event_tracking . '>' . l($stream->comment_count, $node_path) . '</span>';
+        print '<div class="meta"><span class="node_title" ' . $event_tracking . '>' . l($node_title, $node_path) . '</span>';
       }
       else {
         print '<li class="top_content_item">';
